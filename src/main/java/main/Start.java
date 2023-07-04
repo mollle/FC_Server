13bfa5cd5d76666954a7d98754aac0c7190e8944
mollle <leon-moll@gmx.de>
2023-07-04 14:30:15 +0200

    minimal beispiel zeromq empfangen und in sql speichern

--- src/main/java/main/Start.java
+++ src/main/java/main/Start.java
@@ -1,81 +1,27 @@
 package main;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.concurrent.ExecutionException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
+import org.zeromq.ZFrame;
 import org.zeromq.ZMQ;
 import org.zeromq.ZMsg;
 
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.WriteResult;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
-
 import classes.Coordinate;
-import classes.Coordinate.Type;
-import services.FirebaseService;
+import services.SqlService;
 
 public class Start {
 
-	private static final Logger logger = LoggerFactory.getLogger(Start.class);
-
 	private static final int port = 8080;
+
+	private static final Logger logger = LoggerFactory.getLogger(Start.class);
 
 	public static void main(String[] args) {
 
-		logger.info("Server gestartet auf Port " + port);
+		SqlService sqlService = new SqlService();
 
-		FirebaseService.initialize();
-		
-//		try {
-//			Thread.sleep(5000);
-//		} catch (InterruptedException e) {
-//			logger.error("sleep wurde unterbrochen");
-//			e.printStackTrace();
-//		}
-		
-		logger.info("Firebase test...");
-		
-        Coordinate coordinateTest = new Coordinate(51.5074, -0.1278, Type.HIGH, new Date());
-        
-		ApiFuture<WriteResult> futureTest = FirebaseService.saveCoordinate(coordinateTest);
-		
-		try {
-			
-			WriteResult resultTest = futureTest.get();
-//			ZMsg reply = new ZMsg();
-//			reply.add("OK");
-//			reply.send(socket);
-			logger.info("Coordinate erfolgreich gespeichert.");
-		} catch (InterruptedException e) {
-			
-			logger.error("Speichern der Coordinate unterbrochen: " + e.getMessage());
-			e.printStackTrace();
-			Thread.currentThread().interrupt();
-//			ZMsg reply = new ZMsg();
-//			reply.add("Error");
-//			reply.send(socket);
-		} catch (ExecutionException e) {
-//			ZMsg reply = new ZMsg();
-//			reply.add("Error");
-//			reply.send(socket);
-			logger.error("Fehler beim Speichern der Coordinate: " + e.getMessage());
-			e.printStackTrace();
-
-		}
-        
-        logger.info("Firebase test... END");
-
+		sqlService.initTables();
 
 		// Erstelle einen ZMQ-Kontext und einen Socket vom Typ REP
 		try (ZContext context = new ZContext()) {
@@ -84,45 +30,22 @@ public class Start {
 
 			while (!Thread.currentThread().isInterrupted()) {
 
-
 				logger.info("Listening on Port " + port);
 
 				// Warte auf eine Nachricht vom Client
-				ZMsg request = ZMsg.recvMsg(socket);
-				logger.info("Got message:" + request.toString());
+		           ZMsg request = ZMsg.recvMsg(socket);
+		            logger.info("Got message: " + request.toString());
 
-				// Extrahiere die Coordinate aus der Nachricht
-				byte[] coordinateBytes = request.getLast().getData();
-				Coordinate coordinate = Coordinate.fromBytes(coordinateBytes);
+		            // Extrahiere den Inhalt der Nachricht als Byte-Array
+		            ZFrame contentFrame = request.getLast();
+		            byte[] messageBytes = contentFrame.getData();
+		            
+		            // Speichere die Coordinate
+		            sqlService.saveCoordinate(Coordinate.fromBytes(messageBytes));
 
-				// Speichere die Coordinate in Firebase
-				ApiFuture<WriteResult> future = FirebaseService.saveCoordinate(coordinate);
-				try {
-					
-					WriteResult result = future.get();
-//					ZMsg reply = new ZMsg();
-//					reply.add("OK");
-//					reply.send(socket);
-					logger.info("Coordinate erfolgreich gespeichert.");
-				} catch (InterruptedException e) {
-					
-					logger.error("Speichern der Coordinate unterbrochen: " + e.getMessage());
-					e.printStackTrace();
-					Thread.currentThread().interrupt();
-//					ZMsg reply = new ZMsg();
-//					reply.add("Error");
-//					reply.send(socket);
-				} catch (ExecutionException e) {
-//					ZMsg reply = new ZMsg();
-//					reply.add("Error");
-//					reply.send(socket);
-					logger.error("Fehler beim Speichern der Coordinate: " + e.getMessage());
-					e.printStackTrace();
-
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		            // Sende eine Bestätigung an den Client
+		            request.send(socket);
+		        }
 		}
 	}
 }
---
+++ src/main/java/services/SqlService.java
@@ -0,0 +1,132 @@
+package services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import classes.Coordinate;
+
+public class SqlService {
+
+	private static final String sqlContainerIP = "172.17.0.2";
+	
+	private static final String url = "jdbc:sqlserver://"+sqlContainerIP+";databaseName=master";
+
+	private static final String username = "sa";
+
+	private static final String password = "notAdmin.23";
+
+	private static final Logger logger = LoggerFactory.getLogger(SqlService.class);
+
+	private Connection connection;
+
+	public SqlService() {
+
+		Properties properties = new Properties();
+		properties.setProperty("user", username);
+		properties.setProperty("password", password);
+		properties.setProperty("ssl", "true");
+		properties.setProperty("sslTrustServerCertificate", "true");
+
+		logger.info("Connecting to SQL server...");
+		try {
+			this.connection = DriverManager.getConnection(url, username, password);
+			logger.info("Connecting to SQL server... DONE");
+		} catch (SQLException e) {
+			logger.error("Can't connect to sql server");
+			e.printStackTrace();
+		}
+	}
+
+	// erstellt 4 tables für traffic, low, medium, high mit den zeilen latidude,
+	// longitude und time
+	public void initTables() {
+
+		logger.info("Init tables...");
+
+		try (Statement statement = connection.createStatement()) {
+
+			// Tabelle für TRAFFIC
+			String createTrafficTableQuery = "IF OBJECT_ID('[dbo].[traffic]', 'U') IS NULL\n"
+					+ "CREATE TABLE [dbo].[traffic]\n" + "(\n" + "    [Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,\n"
+					+ "    [Timestamp] DATETIME NOT NULL,\n" + "    [Latitude] FLOAT NOT NULL,\n"
+					+ "    [Longitude] FLOAT NOT NULL\n" + ")";
+			statement.execute(createTrafficTableQuery);
+
+			// Tabelle für LOW
+			String createLowTableQuery = "IF OBJECT_ID('[dbo].[low]', 'U') IS NULL\n" + "CREATE TABLE [dbo].[low]\n"
+					+ "(\n" + "    [Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,\n"
+					+ "    [Timestamp] DATETIME NOT NULL,\n" + "    [Latitude] FLOAT NOT NULL,\n"
+					+ "    [Longitude] FLOAT NOT NULL\n" + ")";
+			statement.execute(createLowTableQuery);
+
+			// Tabelle für MEDIUM
+			String createMediumTableQuery = "IF OBJECT_ID('[dbo].[medium]', 'U') IS NULL\n" + "CREATE TABLE [dbo].[medium]\n"
+					+ "(\n" + "    [Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,\n"
+					+ "    [Timestamp] DATETIME NOT NULL,\n" + "    [Latitude] FLOAT NOT NULL,\n"
+					+ "    [Longitude] FLOAT NOT NULL\n" + ")";
+			statement.execute(createMediumTableQuery);
+
+			// Tabelle für HIGH
+			String createHighTableQuery = "IF OBJECT_ID('[dbo].[high]', 'U') IS NULL\n" + "CREATE TABLE [dbo].[high]\n"
+					+ "(\n" + "    [Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,\n"
+					+ "    [Timestamp] DATETIME NOT NULL,\n" + "    [Latitude] FLOAT NOT NULL,\n"
+					+ "    [Longitude] FLOAT NOT NULL\n" + ")";
+			statement.execute(createHighTableQuery);
+
+			logger.info("Init tables... DONE");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void saveCoordinate(Coordinate coordinate) {
+
+	    // Überprüfe, ob das Coordinate-Objekt erfolgreich erstellt wurde
+	    if (coordinate == null) {
+	        logger.error("Error creating Coordinate object from message");
+	        return;
+	    }
+
+	    // Speichere das Coordinate-Objekt in der entsprechenden Tabelle
+	    String tableName;
+	    switch (coordinate.getType()) {
+	        case TRAFFIC:
+	            tableName = "traffic";
+	            break;
+	        case LOW:
+	            tableName = "low";
+	            break;
+	        case MEDIUM:
+	            tableName = "medium";
+	            break;
+	        case HIGH:
+	            tableName = "high";
+	            break;
+	        default:
+	            logger.error("Invalid coordinate type");
+	            return;
+	    }
+
+	    String insertQuery = "INSERT INTO " + tableName + " (Timestamp, Latitude, Longitude) VALUES (?, ?, ?)";
+	    try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+	        preparedStatement.setTimestamp(1, new Timestamp(coordinate.getTimestamp().getTime()));
+	        preparedStatement.setDouble(2, coordinate.getLatitude());
+	        preparedStatement.setDouble(3, coordinate.getLongitude());
+	        preparedStatement.executeUpdate();
+	        logger.info("Coordinate saved to table: " + tableName);
+	    } catch (SQLException e) {
+	        logger.error("Error saving coordinate to table: " + tableName);
+	        e.printStackTrace();
+	    }
+	}
+
+}
